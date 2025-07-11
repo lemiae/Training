6a0d2c39295edee41275f4da8cb3af1f4e1260c0
Amelie Richioud <leiamellei9@gmail.com>
2025-07-11 19:26:07 +0200

    AI test

---
+++ jeux/main.py
@@ -0,0 +1,14 @@
+# Utiliser l'IA Codeium
+for i in range(10):
+    print(i)
+    if i == 5:
+        break
+
+# Ctrl + i pour le racourci et penser à écrire en anglais la demande
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
