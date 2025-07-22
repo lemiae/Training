cd76ef504e0737c3521a56faa9f79cd9140c55b2
Amelie Richioud <leiamellei9@gmail.com>
2025-07-22 12:21:50 +0200

    End emails part

--- automatisation/emails.py
+++ automatisation/emails.py
@@ -1,4 +1,30 @@
 
 # --- ENOYER DES EMAILS ---
-
+import smtplib
 import emails_config
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def envoyer_email(email_destinataire,sujet, message):
+
+    multipart_message = MIMEMultipart()
+    multipart_message["Subject"] = sujet
+    multipart_message["From"] = emails_config.config_email
+    multipart_message["To"] = email_destinataire
+
+    multipart_message.attach(MIMEText(message, "plain"))
+
+    serveur_mail = smtplib.SMTP(emails_config.config_server, emails_config.config_server_port)
+    serveur_mail.starttls()
+    serveur_mail.login(emails_config.config_email, emails_config.config_password)
+    serveur_mail.sendmail(emails_config.config_email, email_destinataire,  multipart_message.as_string())
+    serveur_mail.quit()
+
+message_email = """Bonjour,
+
+Comment allez-vous ? 
+
+Ceci est un message envoyé depuis Python.
+"""
+
+envoyer_email("amelie.richioud@gmail.com", "Email depuis Python", message_email)
