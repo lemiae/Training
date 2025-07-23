25e3035faeacec8b1b246d90a49a815c6431dfef
Amelie Richioud <leiamellei9@gmail.com>
2025-07-23 11:08:41 +0200

    Scrapping

--- cake_scrap/main.py
+++ cake_scrap/main.py
@@ -14,4 +14,7 @@ if list_div_centre and len(list_div_centre) >= 2:
     paragraphe_description= list_div_centre[1].find("p", class_="description")
 print("Paragraphe de la description :", paragraphe_description.text)
 
+div_info = soup.find("div", class_="info")
+img_info = div_info.find("img")
+print("Image de la recette :", img_info["src"])
 print()
